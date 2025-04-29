5e9667b39fcc1e44d12f2868379868139111696e
MackInSpace <amackey@lssu.edu>
2025-04-29 11:39:58 -0600

    updated app to reflect module changes

--- node-examples/app.js
+++ node-examples/app.js
@@ -7,12 +7,15 @@ const rect = require('./rectangle');
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-    if (l <= 0 || w <= 0) {
-        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
-    } else {
-        console.log(`Area of rectangle: ${rect.area(l, w)}`);
-        console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-    }
+    rect(l, w, (err, result) => {
+        if (err) {
+            console.log('ERROR:', err.message);
+        } else {
+            console.log(`Area of rectangle with dimensions: ${l}, ${w} is ${result.area()}.`);
+            console.log(`Perimeter of rectangle with dimensions: ${l}, ${w} is ${result.perimeter()}.`);
+        }
+    });
+    console.log('This statement is logged after the call to rect()');
 }
 
 solveRect(2, 4);
