7cfce3abf7f23df42bc2febf3f1c13147ae4b1cf
MackInSpace <amackey@lssu.edu>
2025-05-18 10:39:06 -0600

    task 4

--- nucampsiteServer/routes/campsiteRouter.js
+++ nucampsiteServer/routes/campsiteRouter.js
@@ -169,19 +169,19 @@ Campsite.findById(req.params.campsiteId)
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
-            if (req.body.rating) {
-                campsite.comments.id(req.params.commentId).rating = req.body.rating;
+            if (campsite.comments.id(req.params.commentId).author.equals(req.user._id)) {
+                if (req.body.rating) {
+                    campsite.comments.id(req.params.commentId).rating = req.body.rating;
+                }
+                if (req.body.text) {
+                    campsite.comments.id(req.params.commentId).text = req.body.text;
+                }
+                return campsite.save()
+            } else {
+                const err = new Error('You are not authorized to update this comment.');
+                err.status = 403;
+                return next(err);
             }
-            if (req.body.text) {
-                campsite.comments.id(req.params.commentId).text = req.body.text;
-            }
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
         } else if (!campsite) {
             err = new Error(`Campsite ${req.params.campsiteId} not found`);
             err.status = 404;
@@ -192,20 +192,25 @@ Campsite.findById(req.params.campsiteId)
             return next(err);
         }
     })
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
     .catch(err => next(err));
 })
 .delete(authenticate.verifyUser, (req, res, next) => {
 Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
-            campsite.comments.id(req.params.commentId).remove();
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
+            if (campsite.comments.id(req.params.commentId).author.equals(req.user._id)) {
+                campsite.comments.id(req.params.commentId).remove();
+                return campsite.save()
+            } else {
+                const err = new Error('You are not authorized to delete this comment.');
+                err.status = 403;
+                return next(err);
+            }
         } else if (!campsite) {
             err = new Error(`Campsite ${req.params.campsiteId} not found`);
             err.status = 404;
@@ -215,6 +220,11 @@ Campsite.findById(req.params.campsiteId)
             err.status = 404;
             return next(err);
         }
+    })
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
     })
     .catch(err => next(err));
 });
